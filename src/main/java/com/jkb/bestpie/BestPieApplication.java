3c30f66d5461ba55aa39947e8856fae87e8633e6
jws3873 <jws3873@gamil.com>
2024-05-08 23:58:48 +0900

    정원석 commit test

--- src/main/java/com/jkb/bestpie/BestPieApplication.java
+++ src/main/java/com/jkb/bestpie/BestPieApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BestPieApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BestPieApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
