d34ad436d3da8e99d959eaa024dbb78d400eb07b
RadSparkle <wwwartg1@naver.com>
2024-05-08 23:46:11 +0900

    Initial commit

---
+++ src/main/java/com/jkb/bestpie/BestPieApplication.java
@@ -0,0 +1,13 @@
+package com.jkb.bestpie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkb/bestpie/BestPieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkb.bestpie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
