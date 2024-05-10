9e3382ea92ecfa43f45c478555b947b1cd0cc0ea
DESKTOP-TU68PH4\성연 <qortjddus159@naver.com>
2024-05-10 12:59:46 +0900

    test : 성연 노트북 커밋

--- src/main/java/com/jkb/bestpie/api/domain/board/controller/BoardController.java
+++ src/main/java/com/jkb/bestpie/api/domain/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.jkb.bestpie.api.domain.board.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class BoardController {
 
+@GetMapping("/board")
+    public String getBoard(){
 
+    return "test";
+}
 
 
 }
