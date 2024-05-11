8d644e00f4dd83cbc2cda254416bb98724136794
jws3873 <jws3873@gamil.com>
2024-05-11 17:23:09 +0900

    정원석 feat: Community Entity 신규작성

---
+++ src/main/java/com/jkb/bestpie/api/domain/scrap/Entity/Community.java
@@ -0,0 +1,27 @@
+package com.jkb.bestpie.api.domain.scrap.Entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "COMMUNITY")
+@Getter @Setter @NoArgsConstructor
+public class Community {
+    @Id @GeneratedValue
+    @Column(name = "community_id")
+    private Long id;
+
+    private String siteName;
+
+    private String title;
+
+    private String url;
+
+    private LocalDateTime regDate;
+
+}
