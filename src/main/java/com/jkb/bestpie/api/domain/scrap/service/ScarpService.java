c19b4ba99f4e0c8ebadf50bc702936faa48ec452
jws3873 <jws3873@gamil.com>
2024-05-11 20:34:41 +0900

    refactor : 1. CommunityService delete 2. ScarpService save 메서드 추가

--- src/main/java/com/jkb/bestpie/api/domain/scrap/service/ScarpService.java
+++ src/main/java/com/jkb/bestpie/api/domain/scrap/service/ScarpService.java
@@ -1,4 +1,9 @@
 package com.jkb.bestpie.api.domain.scrap.service;
 
+import com.jkb.bestpie.api.domain.scrap.Entity.Community;
+
+import java.util.List;
+
 public interface ScarpService {
+    public void save(Community community);
 }
