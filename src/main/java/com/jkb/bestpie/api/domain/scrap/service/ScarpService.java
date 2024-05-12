468c0df76c22857cc5a338a2fd46768f835c38c5
jws3873 <jws3873@gamil.com>
2024-05-13 00:01:31 +0900

    refactor : 저장메서드 save -> saveScrap

--- src/main/java/com/jkb/bestpie/api/domain/scrap/service/ScarpService.java
+++ src/main/java/com/jkb/bestpie/api/domain/scrap/service/ScarpService.java
@@ -2,8 +2,6 @@ package com.jkb.bestpie.api.domain.scrap.service;
 
 import com.jkb.bestpie.api.domain.scrap.Entity.Community;
 
-import java.util.List;
-
 public interface ScarpService {
-    public void save(Community community);
+    public void saveScrap(Community community);
 }
