d95cd23e58d96e0a90b2a4302f707a0efd250b30
jws3873 <jws3873@gamil.com>
2024-05-13 00:02:29 +0900

    feat : 1. ScraperRepository 추가 2. ScrapServiceImpl : saveScrap 구현

---
+++ src/main/java/com/jkb/bestpie/api/domain/scrap/repository/ScraperRepository.java
@@ -0,0 +1,10 @@
+package com.jkb.bestpie.api.domain.scrap.repository;
+
+import com.jkb.bestpie.api.domain.scrap.Entity.Community;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScraperRepository extends JpaRepository<Community, Long> {
+    boolean existsBySiteNameAndTitle(String siteName, String title);
+}
--- src/main/java/com/jkb/bestpie/api/domain/scrap/service/ScrapServiceImpl.java
+++ src/main/java/com/jkb/bestpie/api/domain/scrap/service/ScrapServiceImpl.java
@@ -1,4 +1,20 @@
 package com.jkb.bestpie.api.domain.scrap.service;
 
-public class ScrapServiceImpl {
+import com.jkb.bestpie.api.domain.scrap.Entity.Community;
+import com.jkb.bestpie.api.domain.scrap.repository.ScraperRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScrapServiceImpl implements ScarpService{
+
+    private final ScraperRepository scraperRepository;
+
+    @Override
+    public void saveScrap(Community community) {
+        if(!scraperRepository.existsBySiteNameAndTitle(community.getSiteName(), community.getTitle())){
+            scraperRepository.save(community);
+        }
+    }
 }
