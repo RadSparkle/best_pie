11a42ceb121411681fa632b99990e75ec819f199
jws3873 <jws3873@gamil.com>
2024-05-12 23:58:13 +0900

    refactor : Community Entity unique key 추가(siteName, title)

--- src/main/java/com/jkb/bestpie/api/domain/scrap/Entity/Community.java
+++ src/main/java/com/jkb/bestpie/api/domain/scrap/Entity/Community.java
@@ -8,19 +8,14 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "COMMUNITY")
+@Table(name = "COMMUNITY", uniqueConstraints = {@UniqueConstraint(columnNames = {"site_name", "title"})})
 @Getter @Setter @NoArgsConstructor
 public class Community {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "community_id")
     private Long id;
-
     private String siteName;
-
     private String title;
-
     private String url;
-
     private LocalDateTime regDate;
-
 }
