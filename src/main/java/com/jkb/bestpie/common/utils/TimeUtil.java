38e440f45f28986ff5b8eac11055e860dbe72972
RadSparkle <wwwartg1@naver.com>
2024-05-15 17:45:50 +0900

    Refacotr : TimeUtil의 getLocalDateTime 메서드가 yyyy.MM.dd HH:mm 포맷도 처리할수있도록

--- src/main/java/com/jkb/bestpie/common/utils/TimeUtil.java
+++ src/main/java/com/jkb/bestpie/common/utils/TimeUtil.java
@@ -5,13 +5,25 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Component
 public class TimeUtil {
 
-    private final String YYYYMMDDHHMMSS_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private final String YYYYMMDDHHMMSS_FORMAT_A = "yyyy-MM-dd HH:mm:ss";
+
+    private final String YYYYMMDDHHMMSS_FORMAT_B = "yyyy.MM.dd HH:mm";
 
     public LocalDateTime getLocalDateTime(String date) {
-        return LocalDateTime.parse(date, DateTimeFormatter.ofPattern(YYYYMMDDHHMMSS_FORMAT));
+        if (!date.isEmpty()) {
+            try {
+                // 우선 들어온 문자열이 "yyyy-MM-dd HH:mm:ss" 형식인지 확인
+                return LocalDateTime.parse(date, DateTimeFormatter.ofPattern(YYYYMMDDHHMMSS_FORMAT_A));
+            } catch (DateTimeParseException e) {
+                // "yyyy-MM-dd HH:mm:ss" 형식이 아닌 경우, 새로운 형식으로 파싱
+                return LocalDateTime.parse(date, DateTimeFormatter.ofPattern(YYYYMMDDHHMMSS_FORMAT_B));
+            }
+        }
+        return null;
     }
 }
